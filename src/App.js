6a2a1ad8911afb9a1355e49ab3f7f356b8450704
ishmanWTF <85202088+ishmanwtf@users.noreply.github.com>
2023-11-27 02:32:12 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import logo from './logowide.png';
+import './App.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+    return (
 
-reportWebVitals();
+
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+
+
+    );
+}
+
+export default App;

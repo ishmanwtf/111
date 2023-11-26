fbbb755c0b5741be532410dabaa86d62c7566a2a
ishmanWTF <85202088+ishmanwtf@users.noreply.github.com>
2023-11-27 01:59:36 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+import logo from './logowide.png';
+import './App.css';
+
+function App() {
+    return (
+
+
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+
+
+    );
+}
+
+export default App;
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
+>>>>>>> 36991170c561780d2fc3c01724219e296e65b0fc
